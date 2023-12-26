afbfbc140b0a0933ac68092c69a87cbb4695c7c8
mertduygu <mduygumert@gmail.com>
2023-12-27 00:10:44 +0300

    layout created

---
+++ src/router/index.jsx
@@ -0,0 +1,36 @@
+import { createBrowserRouter } from "react-router-dom";
+import UserLayout from "../layout/user-layout";
+import HomePage from "../pages/home-page";
+import CoursesPage from "../pages/courses-page";
+import EventsPage from "../pages/events-page";
+import AboutPage from "../pages/about-page";
+import ContactPage from "../pages/contact-page";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <UserLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "courses",
+        element: <CoursesPage />,
+      },
+      {
+        path: "events",
+        element: <EventsPage />,
+      },
+      {
+        path: "about",
+        element: <AboutPage />,
+      },
+      {
+        path: "contact",
+        element: <ContactPage />,
+      },
+    ],
+  },
+]);
