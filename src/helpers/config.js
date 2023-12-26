b9ec0d12cf38257e05564a5efdf9262cb58e5e96
mertduygu <mduygumert@gmail.com>
2023-12-27 02:16:56 +0300

    slogan and description added

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,7 +1,7 @@
 export const config = {
     project: {
         name: 'LearnHub',
-        slogan: "📢fhjdkha kuhfajfio fuhaifsjh",
-        description: "",
+        slogan: "📢 LearnHub: Navigating Education, Empowering Futures!",
+        description: "Welcome to LearnHub, where innovation meets education! Our cutting-edge school management app is designed to seamlessly connect students, teachers, and parents in a collaborative learning ecosystem. With LearnHub, stay effortlessly organized with intuitive features for attendance tracking, grade management, and communication tools. Empower your educational journey with real-time insights, interactive learning resources, and streamlined administration. Experience the future of education management – LearnHub, transforming the way we learn, teach, and succeed together.",
     }
 }
